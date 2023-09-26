f242556819b49e5f46ff0479bfd83982a489d1d1
asma0101 <asma.official602@gmail.com>
2023-09-27 04:47:41 +0500

    update package.json

--- src/app/components/carousal/page.tsx
+++ src/app/components/carousal/page.tsx
@@ -1,8 +1,6 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Carousel = (props:any) => {
   const settings = {
