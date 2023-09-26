74875892929d2dfab940c0da7254104dae34180f
asma0101 <asma.official602@gmail.com>
2023-09-27 04:37:52 +0500

    HOC fix

--- src/app/components/authGaurd/page.tsx
+++ src/app/components/authGaurd/page.tsx
@@ -1,27 +1,33 @@
-
-"use client";
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setErrorAlert } from '../../redux/Actions/userActions';
+import { setErrorAlert } from "@/app/redux/Actions/userActions";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 const authGuard = (WrappedComponent: any) => {
-  return (props:any) => {
-      const router = useRouter();
-      const dispatch = useDispatch();
+  const AuthGuardComponent = (props: any) => {
+    const router = useRouter();
+    const dispatch = useDispatch();
 
     useEffect(() => {
       const token = localStorage.getItem('token');
-        if (token) {
-            return;
-        }
-        else if (!token) {
-            dispatch(setErrorAlert(true, 'Please login to proceed!'));
-            router.push('/auth?view=login'); 
-        }
+      if (token) {
+        return;
+      } else {
+        dispatch(setErrorAlert(true, 'Please login to proceed!'));
+        router.push('/auth?view=login'); 
+      }
     }, [router, dispatch]);
 
     return <WrappedComponent {...props} />;
   };
+
+  AuthGuardComponent.displayName = `AuthGuard(${getDisplayName(WrappedComponent)})`;
+
+  return AuthGuardComponent;
 };
+
+function getDisplayName(WrappedComponent: any) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
 export default authGuard;
